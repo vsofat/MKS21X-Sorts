ac77d6cadc7a50cee2484d4fa3b87a819b2288e6
vsofat <vsofat00@stuy.edu>
2018-12-05 09:26:40 -0500

    selectionSort added

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,23 @@
-public class sort{
+public class Sorts{
   public static int smallestVal(int ary[], int index) {
   int small = ary[index];
   int smallestIndex = index;
-  for (int i = index + 1; x < ary.length; x ++) {
+  for (int i = index + 1; i < ary.length; i ++) {
     if (ary[i] < small) {
       small = ary[i];
       smallestIndex = i;
     }
   }
   return smallestIndex;
+}
+
+public static void selectionSort(int [] ary) {
+  int smallest;
+  // you put the objects in order based off their values, which is why you need smallest
+  for(int i = 0; i < ary.length; i++){
+    smallest = smallestVal(ary,i);
+    ary[i] = smallest;
+    }
+  return ary;
+  }
 }
