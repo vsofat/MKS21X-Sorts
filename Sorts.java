7921ecf81ae6ab6cb5217db6d71771255f53ce82
vsofat <vsofat00@stuy.edu>
2018-12-11 11:36:26 -0500

    selection fixed; insertiona added

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,5 @@
 public class Sorts{
-  public static int smallestVal(int ary[], int index) {
+  /*public static int smallestVal(int ary[], int index) {
   int small = ary[index];
   int smallestIndex = index;
   for (int i = index + 1; i < ary.length; i++) {
@@ -9,16 +9,25 @@ public class Sorts{
     }
   }
   return smallestIndex;
-}
+} */
 
-public static void selectionSort(int [] ary) {
-  int smallest;
-  // you put the objects in order based off their values, which is why you need smallest
-  for(int i = 0; i < ary.length; i++){
-    smallest = smallestVal(ary,i);
-    ary[i] = smallest;
-    }
+public static void selectionSort(int[] ary){
+  if(ary.length!=0){
+  int placeHolder;
+  int indexVal;
+  for(int i = 0; i<ary.length; i++){
+    placeHolder = ary[i];
+    indexVal = i;
+    for(int j = i; j < ary.length; j++){
+      if(ary[j]<placeHolder){
+        placeHolder=ary[j];
+        indexVal=j;
+      }}
+    ary[indexVal]=ary[i];
+    ary[i]=placeHolder;
   }
+}
+}
 
 public static void bubbleSort(int[] data){
     boolean status = false;
@@ -35,4 +44,18 @@ public static void bubbleSort(int[] data){
       }
     }
   }
+  public static void insertionSort(int ary[]) {
+  	    for (int i = 1;  i < ary.length; i ++) {
+  	      int orig = ary[i];
+  	      for (int j = i - 1; j >= 0; j --) {
+  	        if (orig < ary[j]) {
+  	          ary[j + 1] = ary[j];
+  	          ary[j] = orig;
+  	        }
+  	        else {
+  	          j = -1;
+  	        }
+  	      }
+  	    }
+  	  }
 }
